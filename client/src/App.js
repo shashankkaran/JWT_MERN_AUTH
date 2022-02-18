6061cf8cb077f2d4747cc899a3441b98c513645f
Shashank Karan <shashankkaran999@gmail.com>
2022-02-19 02:50:34 +0530

    initialising

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css"
+
+
+const App = (props) => {
+  return (
+    <div className="ml-20">Hello ✌</div>
+  );
+};
+
+export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        eeorange: {
+          300: "#FDBA74",
+          500: "#F97316",
+          700: "#EA580C",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
